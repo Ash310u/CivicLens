96dc48cfbbb4897082d0b4ddffb7a1747853f9f3
Ash310u <ashutoshsaha233@gmail.com>
2026-04-09 01:02:27 +0530

    boilerplate setup for store & RTK-query

---
+++ src/store/store.js
@@ -0,0 +1,30 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { baseApi } from '@/store/api/baseApi';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    token: null,
+  },
+  reducers: {
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+    clearToken: (state) => {
+      state.token = null;
+    },
+  },
+});
+
+export const { setToken, clearToken } = authSlice.actions;
+
+export const store = configureStore({
+  reducer: {
+    auth: authSlice.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
+});
+
+setupListeners(store.dispatch);
